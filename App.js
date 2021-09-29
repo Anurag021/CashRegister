eb432bf21a7d60e4e53b6741ee206dcc70db9c7b
Anurag021 <anu.rawat@outlook.com>
2021-09-29 23:08:22 +0530

    Code Updated:
    Added type to variable and disable property

--- App.js
+++ App.js
@@ -7,32 +7,46 @@ const noOfNotes = document.querySelectorAll(".no-Of-Notes")
 
 var currency = [2000,500,100,20,10,5,1];
 
+
+cashGiven.disabled= true
+billAmount.addEventListener("change", enableCashGiven)
+function enableCashGiven(){
+     cashGiven.disabled = false
+ }
+
+
 btnCheck.addEventListener("click", clickHandler)
 
+
 function clickHandler () {
+    cashProvided = Number(cashGiven.value)
+    Bill = Number(billAmount.value)
 // for printing error message
 console.log("clicked")
-if(isNaN(billAmount.value)){
+if(isNaN(Bill)){
     errorMessage.style.display = "block"; 
     errorMessage.innerText = "Input amount is not a number"
 }else{
-if (billAmount.value>0){
-    if(cashGiven.value>=billAmount.value){
+if (Bill > 0){
+    if(cashProvided >= Bill){
         errorMessage.style.display = "none"; 
         console.log("processing");
-        var balanceAmount = cashGiven.value - billAmount.value;
+        var balanceAmount = cashProvided - Bill;
         CalculateChange(balanceAmount)
 
     }else {
         errorMessage.style.display = "block"; 
         errorMessage.innerText = "Do you wanna wash plates ?"}
-} else { 
+} 
+else { 
     errorMessage.innerText = "Invalid amount entered"
     errorMessage.style.display = "block";
-}}}
+}
+}
+}
     function CalculateChange(balanceAmount){
-    console.log(cashGiven.value);
-    var balanceAmount = cashGiven.value - billAmount.value ;
+    console.log(cashProvided);
+    var balanceAmount = cashProvided - Bill ;
     var i = 0;
     while (balanceAmount > 0){
         const numberOfNotes = Math.trunc(balanceAmount / currency[i]);
